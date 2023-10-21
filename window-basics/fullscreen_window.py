c071f7880276ae27609fb9564e46122f8ebad324
Joj Macho <zingakay@gmail.com>
2023-10-21 09:50:38 +0200

    Change file name

---
+++ window-basics/fullscreen_window.py
@@ -0,0 +1,47 @@
+import pygame
+from pygame.locals import *
+import sys
+
+# Constants
+DARK_GRAY = (75, 75, 75)  # Color constant for dark grey color
+WINDOW_WIDTH = 800  # Width of the window
+WINDOW_HEIGHT = 600  # Height of the window
+FRAMES_PER_SECOND = 30  # Frame rate for the game
+
+
+def main():
+    '''Main function to set the Pygame window to fullscreen mode.'''
+    # Initialize Pygame
+    pygame.init()
+
+    # Create a fullscreen window
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
+    pygame.display.set_caption('Fullscreen Window')
+
+    # Create a clock object to control frame rate  
+    clock = pygame.time.Clock()
+
+    # Main loop
+    while True:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == QUIT:  # Exit the main loop if the user wants to quit
+                pygame.quit()
+                sys.exit()
+
+        # Handle game logic
+
+        # Clear the window
+        window.fill(DARK_GRAY)
+
+        # Draw all window elements
+
+        # Update the display
+        pygame.display.update()
+
+        # Control the frame rate
+        clock.tick(FRAMES_PER_SECOND)
+
+
+if __name__ == '__main__':
+    main()
