05de7cfb3b07d550f463b56a3755a4d2f086f516
Joj Macho <zingakay@gmail.com>
2023-11-10 18:31:09 +0200

    Document and fix constants and comments

--- window-basics/blank_window.py
+++ window-basics/blank_window.py
@@ -2,42 +2,36 @@ import pygame
 from pygame.locals import *
 import sys
 
-# Constants
-# BLACK = (0, 0, 0)  
-DARK_GRAY = (75, 75, 75)  # Color constant for dark grey color
+# Colors 
+DARK_GRAY = (75, 75, 75)  # Dark grey color
+
+# Screen Dimensions
 WINDOW_WIDTH = 800  # Width of the window
 WINDOW_HEIGHT = 600  # Height of the window
+
+# Game Settings
 FRAMES_PER_SECOND = 30  # Frame rate for the game
 
 
 def main():
+    '''Main function to set-up a blank window.'''
     # Initialize Pygame
     pygame.init()
 
     # Create a window
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Blank Window')  # Set the window title
-
-    clock = pygame.time.Clock()  # Create a clock object to control frame rate
-
-    # Load assets
-
-    # Intitalize variables
+    clock = pygame.time.Clock()  # Clock object to control frame rate
 
     # Main loop
     while True:
-        # Event handling
-        for event in pygame.event.get():  # Check for events
+        for event in pygame.event.get():  # Handle events
             if event.type == QUIT:  # Exit the main loop if the user wants to quit
                 pygame.quit()
                 sys.exit()
 
-        # Handle game logic
-
-        # Clear the window
-        window.fill(DARK_GRAY)  # Clear the window with a dark-grey background
-
-        # Draw all window elements
+        # Clear the window with a dark-grey background
+        window.fill(DARK_GRAY)
 
         # Update the display
         pygame.display.update()
--- window-basics/fullscreen_window.py
+++ window-basics/fullscreen_window.py
@@ -2,10 +2,14 @@ import pygame
 from pygame.locals import *
 import sys
 
-# Constants
-DARK_GRAY = (75, 75, 75)  # Color constant for dark grey color
+# Colors 
+DARK_GRAY = (75, 75, 75)  # Dark grey color
+
+# Screen Dimensions
 WINDOW_WIDTH = 800  # Width of the window
 WINDOW_HEIGHT = 600  # Height of the window
+
+# Game Settings
 FRAMES_PER_SECOND = 30  # Frame rate for the game
 
 
@@ -14,37 +18,26 @@ def main():
     # Initialize Pygame
     pygame.init()
 
-    # Create a fullscreen window
+    # Create a window
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
-    pygame.display.set_caption('Fullscreen Window')
-
-    # Create a clock object to control frame rate  
-    clock = pygame.time.Clock()
-
-    # Load assets
-
-    # Intitalize variables
+    pygame.display.set_caption('Fullscreen Window')  # Set the window title
+    clock = pygame.time.Clock()  # Clock object to control frame rate
 
     # Main loop
     while True:
-        # Event handling
-        for event in pygame.event.get():
+        for event in pygame.event.get():  # Handle events
             if event.type == QUIT:  # Exit the main loop if the user wants to quit
                 pygame.quit()
                 sys.exit()
 
-        # Handle game logic
-
-        # Clear the window
+        # Clear the window with a dark-grey background
         window.fill(DARK_GRAY)
-
-        # Draw all window elements
 
         # Update the display
         pygame.display.update()
 
         # Control the frame rate
-        clock.tick(FRAMES_PER_SECOND)
+        clock.tick(FRAMES_PER_SECOND)  # Cap the frame rate to maintain a consistent speed
 
 
 if __name__ == '__main__':
